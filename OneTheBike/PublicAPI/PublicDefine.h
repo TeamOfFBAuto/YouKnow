1fda509434e28ea8c255cb76a6dae625b0cdcae1
fblife2014 <shizhongkun@fblife.com>
2014-09-25 00:05:02 +0800

    ss

---
+++ OneTheBike/PublicAPI/PublicDefine.h
@@ -0,0 +1,13 @@
+//
+//  PublicDefine.h
+//  OneTheBike
+//
+//  Created by szk on 14-9-24.
+//  Copyright (c) 2014年 szk. All rights reserved.
+//
+
+#ifndef OneTheBike_PublicDefine_h
+#define OneTheBike_PublicDefine_h
+
+
+#endif
