37abe9bfe8aadb9e5e25833c9873ee489a3b43f5
fblife2014 <shizhongkun@fblife.com>
2014-10-25 17:54:30 +0800

    s
    x

--- OneTheBike/ThirdService/chat/EGO_Refresh/UMEGORefreshTableHeaderView.m
+++ OneTheBike/ThirdService/chat/EGO_Refresh/UMEGORefreshTableHeaderView.m
@@ -41,10 +41,10 @@
 
 - (id)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
-
+        
         self.autoresizingMask = UIViewAutoresizingFlexibleWidth;
         self.backgroundColor = [UIColor clearColor];
-
+        
         UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, frame.size.height - 20.0f, self.frame.size.width, 16.0f)];
         label.autoresizingMask = UIViewAutoresizingFlexibleWidth;
         label.font = [UIFont systemFontOfSize:11.0f];
@@ -56,7 +56,7 @@
         [self addSubview:label];
         _lastUpdatedLabel = label;
         [label release];
-
+        
         label = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, frame.size.height - 40.0f, self.frame.size.width, 16.0f)];
         label.autoresizingMask = UIViewAutoresizingFlexibleWidth;
         label.font = [UIFont systemFontOfSize:12.0f];
@@ -68,43 +68,43 @@
         [self addSubview:label];
         _statusLabel = label;
         [label release];
-
+        
         CALayer *layer = [CALayer layer];
         layer.frame = CGRectMake(30.0f, frame.size.height - 45.0f, 30.0f, 40.0f);
         layer.contentsGravity = kCAGravityResizeAspect;
         layer.contents = (id) [UIImage imageNamed:@"blueArrow"].CGImage;
-
+        
         if ([[UIScreen mainScreen] respondsToSelector:@selector(scale)]) {
             layer.contentsScale = [[UIScreen mainScreen] scale];
         }
-
+        
         [[self layer] addSublayer:layer];
         _arrowImage = layer;
-
+        
         CALayer *umengLogolayer = [CALayer layer];
         umengLogolayer.frame = CGRectMake(frame.size.width / 2 - 60, frame.size.height - 65.0f, 123.0, 17.0);
         umengLogolayer.contentsGravity = kCAGravityResizeAspect;
-        umengLogolayer.contents = (id) [UIImage imageNamed:@"umeng_logo.png"].CGImage;
-
+        umengLogolayer.contents = (id) [UIImage imageNamed:@"logo2.png"].CGImage;
+        
         if ([[UIScreen mainScreen] respondsToSelector:@selector(scale)]) {
             umengLogolayer.contentsScale = [[UIScreen mainScreen] scale];
         }
-
+        
         [[self layer] addSublayer:umengLogolayer];
         _umengLogo = umengLogolayer;
-
+        
         UIActivityIndicatorView *view = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
         view.frame = CGRectMake(30.0f, frame.size.height - 30.0f, 16.0f, 16.0f);
         [self addSubview:view];
         _activityView = view;
         [view release];
-
+        
         [self setState:UMEGOOPullRefreshNormal];
-
+        
     }
-
+    
     return self;
-
+    
 }
 
 
@@ -112,74 +112,74 @@
 #pragma mark Setters
 
 - (void)refreshLastUpdatedDate {
-
+    
     if ([_delegate respondsToSelector:@selector(egoRefreshTableHeaderDataSourceLastUpdated:)]) {
-
+        
         NSDate *date = [_delegate egoRefreshTableHeaderDataSourceLastUpdated:self];
-
+        
         NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
         [formatter setAMSymbol:NSLocalizedString(@"a_m", @"上午")];
         [formatter setPMSymbol:NSLocalizedString(@"p_m", @"下午")];
         [formatter setDateFormat:@"MM.dd ahh:mm"];
-        _lastUpdatedLabel.text = [NSString stringWithFormat:@"%@: %@", NSLocalizedString(@"Last_updated", @"最后更新"),[formatter stringFromDate:date]];
+        _lastUpdatedLabel.text = [NSString stringWithFormat:@"%@: %@", NSLocalizedString(@"最后更新", @"最后更新"),[formatter stringFromDate:date]];
         [[NSUserDefaults standardUserDefaults] setObject:_lastUpdatedLabel.text forKey:@"EGORefreshTableView_LastRefresh"];
         [[NSUserDefaults standardUserDefaults] synchronize];
         [formatter release];
-
+        
     } else {
-
+        
         _lastUpdatedLabel.text = nil;
-
+        
     }
-
+    
 }
 
 - (void)setState:(UMEGOPullRefreshState)aState {
-
+    
     switch (aState) {
         case UMEGOOPullRefreshPulling:
-
-            _statusLabel.text = NSLocalizedString(@"Release_to_refresh", @"松开即可刷新...");
+            
+            _statusLabel.text = NSLocalizedString(@"松开即可更新", @"松开即可刷新...");
             [CATransaction begin];
             [CATransaction setAnimationDuration:FLIP_ANIMATION_DURATION];
             _arrowImage.transform = CATransform3DMakeRotation((M_PI / 180.0) * 180.0f, 0.0f, 0.0f, 1.0f);
             [CATransaction commit];
-
+            
             break;
         case UMEGOOPullRefreshNormal:
-
+            
             if (_state == UMEGOOPullRefreshPulling) {
                 [CATransaction begin];
                 [CATransaction setAnimationDuration:FLIP_ANIMATION_DURATION];
                 _arrowImage.transform = CATransform3DIdentity;
                 [CATransaction commit];
             }
-
-            _statusLabel.text = NSLocalizedString(@"Pull down to refresh", @"下拉可以刷新...");
+            
+            _statusLabel.text = NSLocalizedString(@"下拉可以刷新", @"下拉可以刷新...");
             [_activityView stopAnimating];
             [CATransaction begin];
             [CATransaction setValue:(id) kCFBooleanTrue forKey:kCATransactionDisableActions];
             _arrowImage.hidden = NO;
             _arrowImage.transform = CATransform3DIdentity;
             [CATransaction commit];
-
+            
             [self refreshLastUpdatedDate];
-
+            
             break;
         case UMEGOOPullRefreshLoading:
-
-            _statusLabel.text = NSLocalizedString(@"Loading", @"刷新中...");
+            
+            _statusLabel.text = NSLocalizedString(@"刷新中", @"刷新中...");
             [_activityView startAnimating];
             [CATransaction begin];
             [CATransaction setValue:(id) kCFBooleanTrue forKey:kCATransactionDisableActions];
             _arrowImage.hidden = YES;
             [CATransaction commit];
-
+            
             break;
         default:
             break;
     }
-
+    
     _state = aState;
 }
 
@@ -188,82 +188,82 @@
 #pragma mark ScrollView Methods
 
 - (void)egoRefreshScrollViewDidScroll:(UIScrollView *)scrollView {
-
+    
     if (_state == UMEGOOPullRefreshLoading) {
-
+        
         CGFloat offset = MAX(scrollView.contentOffset.y * -1, 0);
         offset = MIN(offset, HEADERVIEW_HEIGHT - 5);
         scrollView.contentInset = UIEdgeInsetsMake(offset, 0.0f, 0.0f, 0.0f);
-
+        
     }
     else if (scrollView.isDragging) {
-
+        
         BOOL _loading = NO;
         if ([_delegate respondsToSelector:@selector(egoRefreshTableHeaderDataSourceIsLoading:)]) {
             _loading = [_delegate egoRefreshTableHeaderDataSourceIsLoading:self];
         }
-
+        
         if (_state == UMEGOOPullRefreshPulling && scrollView.contentOffset.y > -HEADERVIEW_HEIGHT && scrollView.contentOffset.y < 0.0f && !_loading) {
             [self setState:UMEGOOPullRefreshNormal];
         } else if (_state == UMEGOOPullRefreshNormal && scrollView.contentOffset.y < -HEADERVIEW_HEIGHT && !_loading) {
             [self setState:UMEGOOPullRefreshPulling];
         }
-
+        
         if (scrollView.contentInset.top != 0) {
             scrollView.contentInset = UIEdgeInsetsZero;
         }
-
+        
     }
-
+    
 }
 
 - (void)egoRefreshScrollViewDidEndDragging:(UIScrollView *)scrollView {
-
+    
     BOOL _loading = NO;
     if ([_delegate respondsToSelector:@selector(egoRefreshTableHeaderDataSourceIsLoading:)]) {
         _loading = [_delegate egoRefreshTableHeaderDataSourceIsLoading:self];
     }
-
+    
     if (scrollView.contentOffset.y <= -HEADERVIEW_HEIGHT && !_loading) {
-
+        
         if ([_delegate respondsToSelector:@selector(egoRefreshTableHeaderDidTriggerRefresh:)]) {
             [_delegate egoRefreshTableHeaderDidTriggerRefresh:self];
         }
-
+        
         [self setState:UMEGOOPullRefreshLoading];
         [UIView beginAnimations:nil context:NULL];
         [UIView setAnimationDuration:0.2];
         scrollView.contentInset = UIEdgeInsetsMake(HEADERVIEW_HEIGHT- 5, 0.0f, 0.0f, 0.0f);
         [UIView commitAnimations];
-
+        
     }
-
+    
 }
 
 - (void)egoRefreshScrollViewDataSourceDidFinishedLoading:(UIScrollView *)scrollView {
-
+    
     [UIView beginAnimations:nil context:NULL];
     [UIView setAnimationDuration:.3];
     [scrollView setContentInset:UIEdgeInsetsMake(0.0f, 0.0f, 0.0f, 0.0f)];
     [UIView commitAnimations];
-
+    
     [self setState:UMEGOOPullRefreshNormal];
-
+    
 }
 
 - (void)egoRefreshScrollViewShowLoadingManual:(UIScrollView *)scrollView {
     [self setState:UMEGOOPullRefreshLoading];
-
+    
     [UIView animateWithDuration:0.3
                      animations:^{
                          scrollView.contentInset = UIEdgeInsetsMake(HEADERVIEW_HEIGHT, 0.0f, 0.0f, 0.0f);
                          scrollView.contentOffset = CGPointMake(0, -HEADERVIEW_HEIGHT);
                      }
-    ];
+     ];
 }
 
 - (void)egoRefreshScrollViewDataSourceStartManualLoading:(UIScrollView *)scrollView {
-
+    
     if ([_delegate respondsToSelector:@selector(egoRefreshTableHeaderDidTriggerRefresh:)]) {
         [_delegate egoRefreshTableHeaderDidTriggerRefresh:self];
     }
@@ -274,7 +274,7 @@
 #pragma mark Dealloc
 
 - (void)dealloc {
-
+    
     _delegate = nil;
     _activityView = nil;
     _statusLabel = nil;
