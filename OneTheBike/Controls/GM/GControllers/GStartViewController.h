bd74288430943e0189df09f3903a4982820faa7d
gaomeng <gaomengaz@163.com>
2014-10-25 21:09:05 +0800

    1
    1

--- OneTheBike/Controls/GM/GControllers/GStartViewController.h
+++ OneTheBike/Controls/GM/GControllers/GStartViewController.h
@@ -18,6 +18,8 @@
 
 #import "GstarCanshuViewController.h"
 
+#import "GyundongCanshuModel.h"
+
 @interface GStartViewController : UIViewController<MAMapViewDelegate, AMapSearchDelegate>
 {
     // 点的数组
@@ -34,12 +36,31 @@
     
     //时间跑起来
     Boolean started;
-    NSInteger totalTakt;
-    NSInteger lapTakt;
+    NSInteger _totalTakt;
+    NSInteger _lapTakt;
     Boolean splitted;
     Boolean reset;
     NSInteger splitTimes;
     NSTimer *timer;
+    
+    NSInteger _timerHour;
+    NSInteger _splitTimerHour;
+    
+    
+    NSInteger timerMin ;
+    NSInteger timerSecond ;
+    
+    
+    NSInteger splitTimerMin ;
+    NSInteger splitTimerSecond ;
+    
+    
+    
+    
+//    BOOL _kaishiyundong;//用于判断 别的类有mapview 回到这个类的时候在viewWillApear方法里是否开启定位
+    
+    NSString *_startTime;//开始时间
+    NSString *_endTime;//结束时间
     
 }
 @property (nonatomic, strong) MAMapView *mapView;
@@ -50,6 +71,8 @@
 @property (nonatomic, retain) MAPolyline* routeLine;
 @property (nonatomic, retain) MAPolylineView* routeLineView;
 
+@property(nonatomic,strong) NSMutableArray *routeLineArray;
+
 
 //路书=================
 /* 起始点经纬度. */
@@ -57,8 +80,17 @@
 /* 终点经纬度. */
 @property (nonatomic) CLLocationCoordinate2D destinationCoordinate;
 
+//运动参数model
+@property(nonatomic,strong)GyundongCanshuModel *gYunDongCanShuModel;
+
+//四个自定义view
+@property(nonatomic,strong)NSArray *fourCustomView;
+
 
 //清理 地图 搜索服务的相关代理
 - (void)returnAction;
 
+//设置参数
+-(void)setImage:(UIImage*)theImage andContent:(NSString *)theStr andDanwei:(NSString *)theDanwei withTag:(NSInteger)theTag
+       withType:(NSString *)theViewType;
 @end
